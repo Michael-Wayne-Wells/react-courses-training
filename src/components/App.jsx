88b8d7c548041e5bee6f2684d2af7538af9419c8
Michael Wells <mwells1286@gmail.com>
2020-03-06 21:10:53 -0800

    add manage course page routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-import HomePage from './home/HomePage.jsx';
-import AboutPage from './about/AboutPage.jsx';
-import Header from './common/Header.jsx';
-import PageNotFound from './PageNotFound.jsx';
-import CoursesPage from './courses/CoursesPage.jsx';
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./home/HomePage.jsx";
+import AboutPage from "./about/AboutPage.jsx";
+import Header from "./common/Header.jsx";
+import PageNotFound from "./PageNotFound.jsx";
+import CoursesPage from "./courses/CoursesPage.jsx";
+import ManageCoursePage from "./courses/ManageCoursePage";
 
 function App() {
-    return (
-      <div className="container-fluid">
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/courses" component={CoursesPage} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </div>
-    );
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
